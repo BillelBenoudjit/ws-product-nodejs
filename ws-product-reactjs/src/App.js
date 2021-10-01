e75d40944c16c98f89865c74c921be1e13a8fc07
BillelBenoudjit <hb_benoudjit@esi.dz>
2021-10-01 08:51:30 +0100

    Get data from the api

--- ws-product-reactjs/src/App.js
+++ ws-product-reactjs/src/App.js
@@ -2,18 +2,47 @@ import axios from "axios";
 import { useEffect, useState } from 'react'
 
 import Chart from "./components/charts/Chart"
-import DataTable from "./components/charts/DataTable"
+import DataTable from "./components/dataTable/DataTable"
 import GeoVisualization from "./components/geoVisualization/GeoVisualization"
+
+import { getHourlyStats, getDailyStats, getHourlyEvents, getDailyEvents, getPoi } from "./services/services"
 
 import './App.css';
 
 function App() {
   const [hourlyEvents, setHourlyEvents] = useState({})
   const [dailyEvents, setDailyEvents] = useState({})
-  const [hourlyStats, setHourlyStats] = useState({})
+  const [hourlyStats, setHourlyStats] = useState([])
   const [dailyStats, setDailyStats] = useState({})
   const [stats, setStats] = useState({})
   const [poi, setPoi] = useState({})
+
+  const servicesApi = async () => {
+    try {
+      await getHourlyStats().then(({ data }) => {
+        setHourlyStats(data)
+      })
+
+      await getDailyStats().then(({ data }) => {
+        setDailyStats(data)
+      })
+
+      await getHourlyEvents().then(({ data }) => {
+        setHourlyEvents(data)
+      })
+
+      await getDailyEvents().then(({ data }) => {
+        setDailyEvents(data)
+      })
+
+    } catch (error) {
+      console.log(error)
+    }
+  }
+
+  useEffect(() => {
+    servicesApi()
+  }, [])
 
   return (
     < div className="App" >
